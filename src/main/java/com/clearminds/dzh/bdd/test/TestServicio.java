0428e62854d9f2ca0f091d6295f41d60ec2decd9
Diana Zambrano <damari01zambrano@gmail.com>
2020-12-09 13:02:23 -0500

    Insertar y Evidencias

---
+++ src/main/java/com/clearminds/dzh/bdd/test/TestServicio.java
@@ -0,0 +1,21 @@
+package com.clearminds.dzh.bdd.test;
+
+import com.clearminds.dzh.dtos.Estudiante;
+import com.clearminds.dzh.excepciones.BDDException;
+import com.clearminds.dzh.servicios.ServicioEstudiante;
+
+public class TestServicio {
+
+	public static void main(String[] args) {
+		
+		ServicioEstudiante srvEstudiante=new ServicioEstudiante();
+		try {
+			srvEstudiante.insertEstudiante(new Estudiante("Juan","Perez"));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+
+	}
+
+}
--- src/main/java/com/clearminds/dzh/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/dzh/servicios/ServicioEstudiante.java
@@ -1,5 +1,17 @@
 package com.clearminds.dzh.servicios;
 
+import com.clearminds.dzh.dtos.Estudiante;
+import com.clearminds.dzh.excepciones.BDDException;
+
 public class ServicioEstudiante extends ServicioBase {
+
+	public void insertEstudiante(Estudiante estudiante) throws BDDException {
+
+		abrirConexion();
+		System.out.println("Insertando Estudiante");
+		System.out.println(estudiante);
+		cerrarConexion();
+
+	}
 
 }
