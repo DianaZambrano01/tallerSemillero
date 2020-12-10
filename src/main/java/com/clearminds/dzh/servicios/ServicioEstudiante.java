8ce37d00a2b7acad94636bd15b095073edee4a1c
Diana Zambrano <damari01zambrano@gmail.com>
2020-12-10 10:24:07 -0500

    Ejercicio 2

--- src/main/java/com/clearminds/dzh/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/dzh/servicios/ServicioEstudiante.java
@@ -2,14 +2,17 @@ package com.clearminds.dzh.servicios;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import com.clearminds.dzh.dtos.Estudiante;
 import com.clearminds.dzh.excepciones.BDDException;
+import com.clearminds.dzh.utils.DateUtils;
 
 public class ServicioEstudiante extends ServicioBase {
 
 	public void insertEstudiante(Estudiante estudiante) throws BDDException {
-
+         Date date=new Date();
+         String fecha=DateUtils.obtenerFecha(date);
 		abrirConexion();
 		System.out.println("Insertando Estudiante");
 		System.out.println(estudiante);
@@ -18,8 +21,8 @@ public class ServicioEstudiante extends ServicioBase {
 
 		try {
 			stmt = getConexion().createStatement();
-			String sql = "insert into estudiantes(nombre,apellido,edad) values('" + estudiante.getNombre() + "','"
-					+ estudiante.getApellido() + "','" + estudiante.getEdad() + "')";
+			String sql = "insert into estudiantes(nombre,apellido,edad,fecha_modificacion) values('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "','" + estudiante.getEdad()  + "','" + fecha + "')";
 			System.out.println("Script: " + sql);
 
 			stmt.executeUpdate(sql);
@@ -34,6 +37,8 @@ public class ServicioEstudiante extends ServicioBase {
 	}
 
 	public void actualizarEstudiante(Estudiante estudiante) throws BDDException {
+	    Date date=new Date();
+        String fecha=DateUtils.obtenerFecha(date);
 		abrirConexion();
 		System.out.println("Actualizando Estudiante");
 		System.out.println(estudiante);
@@ -43,7 +48,7 @@ public class ServicioEstudiante extends ServicioBase {
 		try {
 			stmt = getConexion().createStatement();
 			String sql = "update estudiantes set nombre='" + estudiante.getNombre() + "', apellido='"
-					+ estudiante.getApellido() + "', edad='" + estudiante.getEdad() + "' where id="
+					+ estudiante.getApellido() + "', edad='" + estudiante.getEdad()  + "', fecha_modificacion='" + fecha+ "' where id="
 					+ estudiante.getId();
 			System.out.println("Script: " + sql);
 
